cbc7442085ca78ab42568ce2e37822f193cab7f6
Eric Chrobak <eric@ericchrobak.com>
2018-04-04 02:16:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(x = " ")
+  puts line_one = " #{x} | #{x} | #{x} "
+  puts line_two = "-----------"
+  puts line_three = " #{x} | #{x} | #{x} "
+  puts line_four = "-----------"
+  puts line_five = " #{x} | #{x} | #{x} "
+end
